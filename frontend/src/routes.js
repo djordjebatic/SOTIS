a1eab523f579f17ac48ef0572976139bb0d43cfd
Djordje Batic <djordjebatic@gmail.com>
2020-12-08 16:42:58 +0100

    graph set up, TODO: functionalities

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -7,6 +7,7 @@ const TakeTest = lazy(() => import('./views/Pages/TakeTest.js'));
 const TestDone = lazy(() => import('./views/Pages/TestDone.js'));
 const Login = lazy(() => import('./views/Pages/LoginAndRegistration/Login/Login.js'));
 const Register = lazy(() => import('./views/Pages/LoginAndRegistration/Registration/Register.js'));
+const Graph = lazy(() => import('./views/Pages/Graph/Graph.js'));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 
@@ -19,8 +20,7 @@ export default  [
   { path: '/newTest', component: NewTest },
   { path: '/takeTest/:id', component: TakeTest },
   { path: '/testDone/:id', component: TestDone },
-
-  //{ path: '/register', component: Register },
+  { path: '/graph', component: Graph },
   //{ path: '/home', component: lazy(() => import('./containers/DefaultLayout')) },
 
 ];
